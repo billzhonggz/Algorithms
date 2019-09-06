70dbc8173961b35ea5e6f21e4ee8aa7538b87916
Bill ZHONG <billzhonggz@outlook.com>
2019-09-06 10:58:12 +0800

    First attempt on Q242.

---
+++ Leetcode-Practices-By-Topic/Sorting/242.valid-anagram.py
@@ -0,0 +1,56 @@
+#
+# @lc app=leetcode id=242 lang=python3
+#
+# [242] Valid Anagram
+#
+# https://leetcode.com/problems/valid-anagram/description/
+#
+# algorithms
+# Easy (53.36%)
+# Likes:    826
+# Dislikes: 116
+# Total Accepted:    387.6K
+# Total Submissions: 726.3K
+# Testcase Example:  '"anagram"\n"nagaram"'
+#
+# Given two strings s and t , write a function to determine if t is an anagram
+# of s.
+# 
+# Example 1:
+# 
+# 
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: s = "rat", t = "car"
+# Output: false
+# 
+# 
+# Note:
+# You may assume the string contains only lowercase alphabets.
+# 
+# Follow up:
+# What if the inputs contain unicode characters? How would you adapt your
+# solution to such case?
+# 
+#
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # First attempt: sort two strings in alphabetical order,
+        # then compare them. If they are the same, return true,
+        # otherwise, return false.
+        # √ Accepted
+        # √ 34/34 cases passed (68 ms)
+        # √ Your runtime beats 38.45 % of python3 submissions
+        # √ Your memory usage beats 6.25 % of python3 submissions (14.4 MB)
+        s = ''.join(sorted(s))
+        t = ''.join(sorted(t))
+        if s == t:
+            return True
+        else:
+            return False
+
